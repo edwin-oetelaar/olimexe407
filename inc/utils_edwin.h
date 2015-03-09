622bf830e730265c5bca0db8d78536e3a3eb8bf8
Edwin van den Oetelaar (i7 VM) <edwin@oetelaar.com>
2015-03-09 14:32:29 +0100

    usart6 is now controlled with Interrupts and a Queue. Input and Output via Serial->USB now working (also on UEXT connector)

---
+++ inc/utils_edwin.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_EDWIN_H_INCLUDED
+#define UTILS_EDWIN_H_INCLUDED
+
+
+/* The prototype shows it is a naked function - in effect this is just an
+assembly function. */
+static void HardFault_Handler( void ) __attribute__( ( naked ) );
+
+
+#endif /* UTILS_EDWIN_H_INCLUDED */
