2ed7e667d3ff0f4999a7cdb190388ac0abe0f705
Edwin van den Oetelaar <edwin@oetelaar.com>
2015-03-10 22:03:16 +0100

    div updates, incl setup of i2c port 1

--- src/main.c
+++ src/main.c
@@ -71,7 +71,7 @@ void SERIAL_write(const char *s, uint32_t len)
     USART_ITConfig(USART6, USART_IT_TXE, ENABLE);
 }
 
-void hardware_init_LED_GPIO()
+void init_LED_GPIO()
 {
 // enable clocks
 // enable pin
@@ -91,7 +91,7 @@ void hardware_init_LED_GPIO()
 
 
 /* debug info on USART 6 connected to USB */
-void USART6_hardware_init(uint32_t baudrate)
+void init_USART6(uint32_t baudrate)
 {
     GPIO_InitTypeDef GPIO_InitStruct; // this is for the GPIO pins used as TX and RX
     USART_InitTypeDef USART_InitStruct; // this is for the USART2 initialization
@@ -135,6 +135,48 @@ void USART6_hardware_init(uint32_t baudrate)
     NVIC_Init(&NVIC_InitStructure);
 
     USART_Cmd(USART6, ENABLE); //Enable USART2
+}
+
+// i2c PB8 en PB9
+void init_I2C1(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct;
+    I2C_InitTypeDef I2C_InitStruct;
+
+    /* init the struct with defaults */
+    GPIO_StructInit(&GPIO_InitStruct);
+    I2C_StructInit(&I2C_InitStruct);
+    /* enable gpio PB8 en PB9 as AF */
+    GPIO_InitStruct.GPIO_Pin =GPIO_Pin_8 | GPIO_Pin_9;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_OD;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOB,&GPIO_InitStruct);
+    /* map AF pins to gpio */
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_I2C1);
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_I2C1);
+    // enable clocks gpio
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB,ENABLE);
+
+
+    /* Enable the CODEC_I2C peripheral clock */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1, ENABLE);
+
+    /* CODEC_I2C peripheral configuration */
+    I2C_DeInit(I2C1);
+    I2C_InitStruct.I2C_Mode = I2C_Mode_I2C;
+    I2C_InitStruct.I2C_DutyCycle = I2C_DutyCycle_2;
+    I2C_InitStruct.I2C_OwnAddress1 = 0x33;
+    I2C_InitStruct.I2C_Ack = I2C_Ack_Enable;
+    I2C_InitStruct.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+    I2C_InitStruct.I2C_ClockSpeed = 100000;
+    /* Enable the I2C peripheral */
+    I2C_Cmd(I2C1, ENABLE);
+    I2C_Init(I2C1, &I2C_InitStruct);
+
+
+
 }
 
 
@@ -183,11 +225,21 @@ void vTimerCallback(void *ptr)
 
 int main(void)
 {
-    QUEUE_INIT(my_TX_queue);
-    QUEUE_INIT(my_RX_queue);
+    QUEUE_INIT(my_TX_queue); // usart6 serial
+    QUEUE_INIT(my_RX_queue); // usart6 serial
 
-    hardware_init_LED_GPIO();
-    USART6_hardware_init(baudrate);
+    init_USART6(baudrate);
+    SERIAL_puts("USART6 init done\r\n");
+    SERIAL_puts("GPIO init\r\n");
+    init_LED_GPIO();
+    SERIAL_puts("GPIO init done\r\n");
+
+
+    SERIAL_puts("I2C init\r\n");
+// we nemen PB9 en PB8 I2C1 omdat die op UEXT zitten
+    init_I2C1();
+    SERIAL_puts("I2C init done\r\n");
+
 
     TimerHandle_t xSecondenTimer;
 
