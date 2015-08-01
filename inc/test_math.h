65d28167d06d2aa984fbbd6d97a7229e3864a397
Edwin van den Oetelaar (i7 VM) <edwin@oetelaar.com>
2015-08-01 13:11:25 +0200

    math test added refactoring, moved USART6 code into utils from main

---
+++ inc/test_math.h
@@ -0,0 +1,10 @@
+//
+// Created by oetelaar on 8/1/15.
+//
+
+#ifndef OLIMEXE407_TEST_MATH_H
+#define OLIMEXE407_TEST_MATH_H
+
+int32_t test_math(void) ;
+
+#endif //OLIMEXE407_TEST_MATH_H
--- inc/utils_edwin.h
+++ inc/utils_edwin.h
@@ -1,10 +1,14 @@
 #ifndef UTILS_EDWIN_H_INCLUDED
 #define UTILS_EDWIN_H_INCLUDED
 
-
+#include "my_queue.h"
+#include <inttypes.h>
 /* The prototype shows it is a naked function - in effect this is just an
 assembly function. */
 static void HardFault_Handler( void ) __attribute__( ( naked ) );
-
-
+void SERIAL_puts(const char *s);
+void SERIAL_write(const char *s, uint32_t len);
+static void init_USART6(uint32_t baudrate);
+// void USART6_IRQHandler(void);
+void init_SERIAL(uint32_t baudrate);
 #endif /* UTILS_EDWIN_H_INCLUDED */
